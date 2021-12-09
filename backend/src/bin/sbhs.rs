725b0281e5a9470e747b33f14f24d19b2e5c4458
Klim Tsoutsman <klimusha@gmail.com>
2021-12-09 18:31:17 +1100

    Add basic authorisation code
    Signed-off-by: Klim Tsoutsman <klimusha@gmail.com>

---
+++ backend/src/bin/sbhs.rs
@@ -0,0 +1,31 @@
+lazy_static::lazy_static! {
+    static ref HTTP_CLIENT: reqwest::Client = reqwest::Client::new();
+}
+
+#[derive(serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct JsonResponse {
+    student_id: String,
+    // The response contains more fields, but the student_id is the only one we
+    // care about. The struct will still deserialize successfully despite the additional
+    // fields in the response.
+}
+
+async fn sbhs_token_to_id(token: String) -> Result<String, Box<dyn std::error::Error>> {
+    const API_ENDPOINT: &str = "https://student.sbhs.net.au/api/details/userinfo";
+
+    let body = HTTP_CLIENT
+        .get(API_ENDPOINT)
+        .header("Authorization", format!("Bearer {}", token))
+        .send()
+        .await?
+        .json::<JsonResponse>()
+        .await?;
+
+    Ok(body.student_id)
+}
+
+#[tokio::main]
+async fn main() {
+    backend::run(sbhs_token_to_id).await;
+}
--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,5 +1,14 @@
-trait Server {
-    fn token_to_id(token: String) -> String;
+use std::future::Future;
 
-    fn run() {}
+/// Entry point for the server.
+///
+/// Takes in a single paramater, `token_to_id`, a function which converts an
+/// unverified token into some verified ID. For example, an OAuth access token
+/// into a user ID.
+pub async fn run<F, Fut>(token_to_id: F)
+where
+    F: Fn(String) -> Fut,
+    Fut: Future<Output = Result<String, Box<dyn std::error::Error>>>,
+{
+    todo!();
 }
