16398453443d2eed06d2142827d85a380283e6fd
Klim Tsoutsman <klimusha@gmail.com>
2021-12-12 14:44:46 +1100

    Implement `from_request` for `Id`
    Signed-off-by: Klim Tsoutsman <klimusha@gmail.com>

--- backend/src/extractor.rs
+++ backend/src/extractor.rs
@@ -13,6 +13,26 @@ pub struct Id<F>(
 where
     F: Fn(String) -> FuncReturn + Clone;
 
+impl<F> From<String> for Id<F>
+where
+    F: Fn(String) -> FuncReturn + Clone,
+{
+    #[inline]
+    fn from(id: String) -> Self {
+        Self(id, PhantomData)
+    }
+}
+
+impl<F> From<Id<F>> for String
+where
+    F: Fn(String) -> FuncReturn + Clone,
+{
+    #[inline]
+    fn from(id: Id<F>) -> Self {
+        id.0
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct IdConfig<F>
 where
@@ -26,14 +46,15 @@ where
     F: Fn(String) -> FuncReturn + Clone,
 {
     fn default() -> Self {
-        panic!("No ID extractor specified");
+        unreachable!("No ID extractor specified");
     }
 }
 
 impl<F> actix_web::FromRequest for Id<F>
 where
-    F: Fn(String) -> FuncReturn + Clone + 'static,
+    F: Fn(String) -> FuncReturn + Clone + 'static + Send + Sync,
 {
+    // TODO
     type Error = ();
 
     type Future = BoxFuture<Result<Self, Self::Error>>;
@@ -41,10 +62,35 @@ where
     type Config = IdConfig<F>;
 
     #[inline]
-    fn from_request(
-        _req: &actix_web::HttpRequest,
-        _payload: &mut actix_web::dev::Payload,
-    ) -> Self::Future {
-        todo!();
+    fn from_request(req: &actix_web::HttpRequest, _: &mut actix_web::dev::Payload) -> Self::Future {
+        let token = match req.headers().get("Authorization") {
+            Some(t) => match t.to_str() {
+                Ok(t) => {
+                    if t.starts_with("Bearer ") {
+                        t.trim_start_matches("Bearer ").to_owned()
+                    } else {
+                        // The authorization field doesn't contain a token e.g. Basic authorization
+                        return Box::pin(futures::future::ready(Err(())));
+                    }
+                }
+                // Non-ASCII characters
+                Err(_) => return Box::pin(futures::future::ready(Err(()))),
+            },
+            // No authorization header
+            None => return Box::pin(futures::future::ready(Err(()))),
+        };
+        let f = match req.app_data::<IdConfig<F>>() {
+            // TODO is cloning here expensive?
+            Some(f) => f.clone(),
+            None => unreachable!("No ID extractor specified"),
+        };
+
+        let result = async move {
+            (f.token_to_id_function)(token)
+                .await
+                .map(Self::from)
+                .map_err(|_| ())
+        };
+        Box::pin(result)
     }
 }
