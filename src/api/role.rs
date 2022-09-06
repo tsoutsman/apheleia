9df6a7c24c4df5c041bf55ce042f3739341da1d9
Klim Tsoutsman <klim@tsoutsman.com>
2022-09-06 23:02:08 +1000

    Add ability to modify role permissions
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- src/api/role.rs
+++ src/api/role.rs
@@ -1,14 +1,18 @@
 use crate::{
     auth::User,
-    Root,
-    db::{model, schema::role, tokio::AsyncRunQueryDsl, DbPool},
+    db::{
+        model,
+        schema::{role, role_permissions},
+        tokio::AsyncRunQueryDsl,
+        DbPool,
+    },
     id::{self, Id},
-    Result,
+    Result, Root,
 };
 
 use actix_web::{delete, get, post, put, web, HttpResponse, Responder};
-use diesel::QueryDsl;
-use serde::{Serialize, Deserialize};
+use diesel::{ExpressionMethods, QueryDsl};
+use serde::{Deserialize, Serialize};
 
 pub(crate) fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(get_role)
@@ -16,6 +20,22 @@ pub(crate) fn config(cfg: &mut web::ServiceConfig) {
         .service(add_role)
         .service(modify_role)
         .service(delete_role);
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub(crate) struct ArchetypePermission {
+    id: Id<id::Archetype>,
+    meta: bool,
+    loan: bool,
+    receive: bool,
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub(crate) struct GetRoleResponse {
+    pub(crate) id: Id<id::Role>,
+    pub(crate) name: String,
+    pub(crate) subject_area: Id<id::SubjectArea>,
+    pub(crate) permissions: Vec<ArchetypePermission>,
 }
 
 #[get("/roles/{id}")]
@@ -29,7 +49,27 @@ async fn get_role(
         .first::<model::Role>(&pool)
         .await?;
 
-    Result::Ok(HttpResponse::Ok().json(role))
+    let permissions = role_permissions::table
+        .filter(role_permissions::role.eq(*role_id))
+        .load::<model::RolePermission>(&pool)
+        .await?
+        .into_iter()
+        .map(|role_permission| ArchetypePermission {
+            id: role_permission.archetype,
+            meta: role_permission.meta,
+            loan: role_permission.loan,
+            receive: role_permission.receive,
+        })
+        .collect();
+
+    let resp = GetRoleResponse {
+        id: role.id,
+        name: role.name,
+        subject_area: role.subject_area,
+        permissions,
+    };
+
+    Result::Ok(HttpResponse::Ok().json(resp))
 }
 
 #[get("/roles")]
@@ -43,6 +83,7 @@ async fn get_roles(pool: web::Data<DbPool>, _: User) -> impl Responder {
 pub(crate) struct AddRole {
     pub(crate) name: String,
     pub(crate) subject_area: Id<id::SubjectArea>,
+    pub(crate) permissions: Vec<ArchetypePermission>,
 }
 
 #[derive(Clone, Debug, Serialize)]
@@ -73,18 +114,42 @@ async fn add_role(
             .execute(&pool)
             .await?;
 
+        for permission in request.permissions {
+            // TODO: Don't do in loop
+            let permission = model::RolePermission {
+                role: id,
+                archetype: permission.id,
+                meta: permission.meta,
+                loan: permission.loan,
+                receive: permission.receive,
+            };
+            diesel::insert_into(role_permissions::table)
+                .values(permission)
+                .execute(&pool)
+                .await?;
+        }
+
         Result::Ok(HttpResponse::Ok().json(AddRoleResponse { id }))
     } else {
         Result::Ok(HttpResponse::Forbidden().finish())
     }
 }
 
-#[derive(Clone, Debug, Deserialize, AsChangeset)]
-#[diesel(table_name = role)]
+#[derive(Clone, Debug, Deserialize)]
 struct ModifyRole {
     name: Option<String>,
     subject_area: Option<Id<id::SubjectArea>>,
+    permissions: Option<Vec<ArchetypePermission>>,
 }
+
+#[derive(Clone, Debug, AsChangeset)]
+#[diesel(table_name = role)]
+struct ModifyRoleChangeset {
+    name: Option<String>,
+    subject_area: Option<Id<id::SubjectArea>>,
+}
+
+// TODO: Add way to modify role permissions
 
 #[put("/roles/{id}")]
 async fn modify_role(
@@ -98,7 +163,31 @@ async fn modify_role(
     if user.is_root(*root.into_inner()) || user.is_admin_of(&pool, role_subject_area).await? {
         let request = request.into_inner();
         let target = role::table.find(*role_id);
-        diesel::update(target).set(request).execute(&pool).await?;
+        let changeset = ModifyRoleChangeset {
+            name: request.name,
+            subject_area: request.subject_area,
+        };
+        diesel::update(target).set(changeset).execute(&pool).await?;
+        if let Some(permissions) = request.permissions {
+            diesel::delete(role_permissions::table)
+                .filter(role_permissions::role.eq(*role_id))
+                .execute(&pool)
+                .await?;
+            for permission in permissions {
+                // TODO: Don't do in loop
+                let permission = model::RolePermission {
+                    role: *role_id,
+                    archetype: permission.id,
+                    meta: permission.meta,
+                    loan: permission.loan,
+                    receive: permission.receive,
+                };
+                diesel::insert_into(role_permissions::table)
+                    .values(permission)
+                    .execute(&pool)
+                    .await?;
+            }
+        }
         Result::Ok(HttpResponse::Ok())
     } else {
         Result::Ok(HttpResponse::Forbidden())
